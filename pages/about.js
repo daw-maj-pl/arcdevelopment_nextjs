75f1e9b743ef1393583e04ab213f51efd2d8f008
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-09-19 20:18:34 +0200

    Create About component

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,134 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-export default function About() {
+import history from '../assets/history.svg';
+
+import CallToAction from './ui/CallToAction';
+
+const useStyles = makeStyles(theme => ({
+  missionStatement: {
+    fontStyle: 'italic',
+    fontWeight: 300,
+    fontSize: '1.5rem',
+    maxWidth: '50em',
+    lineHeight: 1.4
+  },
+  rowContainer: {
+    padding: '0 5em',
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '1.5em',
+      paddingRight: '1.5em'
+    }
+  }
+}));
+
+export default function About(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js v5-beta example
+    <Grid container direction="column">
+      <Grid
+        item
+        className={classes.rowContainer}
+        style={{ marginTop: matchesMD ? '1em' : '2em' }}
+      >
+        <Typography align={matchesMD ? 'center' : undefined} variant="h2">
+          About Us
         </Typography>
-        <Button variant="contained" component={Link} noLinkStyle href="/">
-          Go to the main page
-        </Button>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+      </Grid>
+      <Grid
+        item
+        container
+        justifyContent="center"
+        className={classes.rowContainer}
+        style={{ marginTop: '3em' }}
+      >
+        <Typography
+          variant="h4"
+          align="center"
+          className={classes.missionStatement}
+        >
+          Whether it be person to person, business to consumer, or an individual
+          to their interests, technology is meant to bring us closer to what we
+          care about in the best way possible. Arc Development will use that
+          principle to provide fast, modern, inexpensive, and aesthetic software
+          to the Midwest and beyond.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{ marginTop: '10em', marginBottom: '10em' }}
+        direction={matchesMD ? 'column' : 'row'}
+        alignItems={matchesMD ? 'center' : undefined}
+        justifyContent="space-around"
+      >
+        <Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            style={{ maxWidth: '35em' }}
+            align={matchesMD ? 'center' : undefined}
+          >
+            <Grid item>
+              <Typography variant="h4" gutterBottom>
+                History
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body1"
+                paragraph
+                style={{ fontWeight: 700, fontStyle: 'italic' }}
+              >
+                We're the new kid on the block
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Founded in 2019, we’re ready to get our hands on the world’s
+                business problems.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                It all started with one question: Why aren’t all businesses
+                using available technology? There are many different answers to
+                that question: economic barriers, social barriers, educational
+                barriers, and sometimes institutional barriers.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                We aim to be a powerful force in overcoming these obstacles.
+                Recent developments in software engineering and computing power,
+                compounded by the proliferation of smart phones, has opened up
+                infinite worlds of possibility. Things that have always been
+                done by hand can now be done digitally and automatically, and
+                completely new methods of interaction are created daily. Taking
+                full advantage of these advancements is the name of the game.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                All this change can be a lot to keep up with, and that’s where
+                we come in.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid item container justifyContent="center" lg>
+            <img
+              src={history}
+              alt="quill pen sitting on top of book"
+              style={{ maxHeight: matchesMD ? 200 : '22em' }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue} />
+      </Grid>
+    </Grid>
   );
 }
